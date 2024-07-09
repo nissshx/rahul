343e687a68f6dc89afc5312806c5669dba8282d5
Nishant <nissshhdev@gmail.com>
2024-07-09 13:39:27 +0530

    changed default font

--- app/font.js
+++ app/font.js
@@ -9,3 +9,4 @@ export const noto = localFont({
         
     ],
 }); 
+export default noto
--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Header'
 import Dashboard from './components/Main'
+import noto from './font'
 
 const Page = () => {
   return (
+    <main className={noto.className}>
     <div className='flex h-screen w-screen'>
       <Sidebar className='w-4/12 h-full' />
       <div className='flex flex-col flex-1'>
@@ -13,6 +15,7 @@ const Page = () => {
         <Dashboard className='flex-1 p-6 overflow-auto w-8/12' />
       </div>
     </div>
+    </main>
   )
 }
 
